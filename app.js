a55186bb095ac864779459eb47aa105b3bb59e18
wbluke <pwb0127@naver.com>
2019-06-25 22:04:14 +0900

    [Tutorial] 5. Events

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Luke',
-        job: 'programmer',
-        website: 'https://wbluke.com',
-        websiteTag: '<a href="https://wbluke.com">Luke Website</a>'
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function(time) {
-            return 'Good ' + time + ' ' + this.name;
+        add: function(inc) {
+            this.age += inc;
+        },
+        subtract: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function(event) { // event 파라미터는 자동으로 감지
+            // console.log(event); // event의 모든 속성을 console에서 볼 수 있다. 
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
