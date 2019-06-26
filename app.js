3f88131ab56f63403193daba1d8c210cd01f2d87
wbluke <pwb0127@naver.com>
2019-06-26 15:24:12 +0900

    [Tutorial] 9. Computed Properties

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: '',
-        age: ''
+        age: 20,
+        a: 0,
+        b: 0
     },
     methods: {
-        logName: function() {
-            console.log('You entered your name');
+        // addToA: function() {
+        //     console.log('addToA');
+        //     return this.a + this.age;
+        // },
+        // addToB: function() {
+        //     console.log('addToB');
+        //     return this.b + this.age;
+        // }
+    },
+    computed: {
+        addToA: function() {
+            console.log('addToA');
+            return this.a + this.age;
         },
-        logAge: function() {
-            console.log('You entered your age');
+        addToB: function() {
+            console.log('addToB');
+            return this.b + this.age;
         }
     }
 });
