eb96d1f4454ae6f15324ef66a75834692d0c6606
wbluke <pwb0127@naver.com>
2019-06-26 14:25:54 +0900

    [Tutorial] 7. Keyboard Events

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 25,
-        x: 0,
-        y: 0
+
     },
     methods: {
-        add: function(inc) {
-            this.age += inc;
+        logName: function() {
+            console.log('You entered your name');
         },
-        subtract: function(dec) {
-            this.age -= dec;
-        },
-        updateXY: function(event) { // event 파라미터는 자동으로 감지
-            // console.log(event); // event의 모든 속성을 console에서 볼 수 있다. 
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function() {
-            alert('You Clicked Me!');
+        logAge: function() {
+            console.log('You entered your age');
         }
     }
 });
