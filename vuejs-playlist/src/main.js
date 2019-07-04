38aba79126221f44cfa7956fea23b65e33fe864c
wbluke <pwb0127@naver.com>
2019-07-04 14:38:32 +0900

    [Tutorial] 25. Event Bus

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -1,10 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import Ninjas from './components/Ninjas.vue'
-
-// Vue.component('ninjas', Ninjas);
 
 Vue.config.productionTip = false
+
+export const bus = new Vue();
 
 new Vue({
   render: h => h(App),
