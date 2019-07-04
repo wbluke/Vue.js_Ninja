f63b8eab1bec13a4ec62d5072e7afef04b037637
wbluke <pwb0127@naver.com>
2019-07-04 14:15:38 +0900

    [Tutorial] 24. Events

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <app-header :title="title"></app-header>
+    <app-header :title="title" v-on:changeTitle="updateTitle($event)"></app-header>
     <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
     <app-footer :title="title"></app-footer>
   </div>
@@ -29,6 +29,11 @@ export default {
       ],
       title: "Vue Ninjas"
     };
+  },
+  methods: {
+    updateTitle: function(updatedTitle) {
+      this.title = updatedTitle;
+    }
   }
 };
 </script>
--- vuejs-playlist/src/components/Header.vue
+++ vuejs-playlist/src/components/Header.vue
@@ -18,7 +18,7 @@ export default {
   },
   methods: {
     changeTitle: function() {
-      this.title = "Vue Wizards";
+      this.$emit('changeTitle', 'Vue Wizards');
     }
   }
 };
