36eeb220cee9c8ad1618399666925a5dae7b6143
wbluke <pwb0127@naver.com>
2019-07-05 16:54:07 +0900

    [Tutorial] 32. HTTP Requests

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 
-export const bus = new Vue();
+Vue.use(VueResource);
 
 new Vue({
   render: h => h(App),
