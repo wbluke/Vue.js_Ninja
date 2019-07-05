2c4bda10786dee69891a88c4d40c7d2eee5d2574
wbluke <pwb0127@naver.com>
2019-07-05 15:48:34 +0900

    [Tutorial] 31. Select Box Binding (forms)

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -16,6 +16,10 @@
         <label>Cheese</label>
         <input type="checkbox" value="cheese" v-model="blog.categories"/>
       </div>
+      <label>Author:</label>
+      <select v-model="blog.author">
+        <option v-for="author in authors" :key="author">{{ author }}</option>
+      </select>
     </form>
     <div id="preview">
       <h3>Preview Blog</h3>
@@ -26,6 +30,7 @@
       <ul>
         <li v-for="category in blog.categories" :key="category">{{ category }}</li>
       </ul>
+      <p>Author: {{ blog.author }}</p>
     </div>
   </div>
 </template>
@@ -37,8 +42,10 @@ export default {
       blog: {
         title: "",
         content: "",
-        categories: []
-      }
+        categories: [],
+        author: ""
+      },
+      authors: ['The Net Ninja', 'The Angular Avenger', 'The Vue Vindicator']
     };
   },
   methods: {}
