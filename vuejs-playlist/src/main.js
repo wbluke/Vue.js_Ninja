d9ce31e77fa7de43844230e7bc2ef0c719deefb4
wbluke <pwb0127@naver.com>
2019-07-05 20:07:51 +0900

    [Tutorial] 34. Custom Directives

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -6,6 +6,28 @@ Vue.config.productionTip = false
 
 Vue.use(VueResource);
 
+// Custom directives
+Vue.directive('rainbow', {
+  bind(el, binding, vnode) {
+    el.style.color = "#" + Math.random().toString().slice(2, 8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding, vnode) {
+    if (binding.value == 'wide') {
+      el.style.maxWidth = "1200px";
+    } else if (binding.value == 'narrow') {
+      el.style.maxWidth = "560px";
+    }
+
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
